c0c990e703b7ee8987e35ca444031fa15634343f
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2025-02-26 19:40:50 -0500

    Creación proyecto navigationApp

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import "./global.css"
+
+export default function index() {
+    return (
+        <View className='mt-10'>
+            <Text className='text-3xl font-bold text-blue-500'>Hola nea</Text>
+        </View>
+    )
+}
