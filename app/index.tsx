41ca9a967b9514ae359956824ab2f5c6de5e1068
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2025-02-27 23:14:00 -0500

    Instalación de Nativewind y fuentes

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from 'react-native'
 import "./global.css"
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function index() {
     return (
-        <View className='mt-10'>
-            <Text className='text-3xl font-bold text-blue-500'>Hola nea</Text>
-        </View>
+        <SafeAreaView>
+            <View className='mt-6 mx-2.5'>
+                <Text className='text-2xl font-work-black'>Hola nea</Text>
+                <Text className='text-2xl font-work-medium'>Hola nea</Text>
+                <Text className='text-2xl font-work-light'>Hola nea</Text>
+            </View>
+        </SafeAreaView>
     )
 }
